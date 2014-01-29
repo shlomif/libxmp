af22075782603fa30a5d11adaa25a9ce4eb51cfc
Shlomi Fish <shlomif@shlomifish.org>
2014-01-29 23:11:24 +0200

    Correct a bug in the change of SRC_OBJS.

--- build-libxmp-for-emscripten.bash
+++ build-libxmp-for-emscripten.bash
@@ -4,14 +4,15 @@ mod_dir="mods"
 mod="yonqatan.mod"
 path_to_mod="$HOME/Music/mp3s/Mods"
 
+export CFLAGS="-g -O0"
 if ! test -e "$mod_dir/$mod" ; then
     mkdir -p "$mod_dir"
     cp "$path_to_mod/$mod" "$mod_dir/"
 fi
 
 autoconf && \
-(CFLAGS="-g -O0" emconfigure ./configure) && \
-perl -i.bak -ple 's/\A(\$\(SRC_OBJS\)\s*=)/${1} js_wrappers.o /' src/Makefile && \
+emconfigure ./configure && \
+perl -i.bak -ple 's/\A(SRC_OBJS\s*=)/${1} js_wrappers.o /' src/Makefile && \
 perl -i.bak -ple 's/\ALOBJS\s*=.*/LOBJS = \$(OBJS)/' Makefile && \
 
 TAB=$'\t'
